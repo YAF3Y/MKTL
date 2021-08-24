00e039c108bc569326fded7a4dafe5fc4768814e
ALLHB <74823117+ALLHB@users.noreply.github.com>
2021-08-24 13:39:23 +0300

    Add files via upload

--- David.lua
+++ David.lua
@@ -4535,10 +4535,10 @@ Welcome To Source
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '᥀ Source Channel',url="https://t.me/UUKXX"}},
-{{text = '᥀ Developer',url="t.me/cicci"},{text = '᥀ Tws MKTL',url="https://t.me/AnEeeSBbot"}},{{text= '᥀ Support Source .',url="https://t.me/UUKXX"}}
+{{text = '᥀ Developer',url="t.me/cicci"},{text = '᥀ Tws MKTL',url="https://t.me/AnEeeSBbot"}},{{text= '᥀ Support Source .',url="https://t.me/XTUTU"}}
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/UUKXX&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/XTUTU&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
 --     Source David     --
@@ -11153,7 +11153,7 @@ keyboard.inline_keyboard = {
 {{text = '᥀ قناة السورس',url="t.me/UUKXX"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/UUKXX&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/XTUTU&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
 --     Source David     --
