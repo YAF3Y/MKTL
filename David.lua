078c41db0712aa0c74fb6edce5455bcc54e23e80
ALLHB <74823117+ALLHB@users.noreply.github.com>
2021-10-22 02:42:50 +0300

    Add files via upload

--- David.lua
+++ David.lua
@@ -9947,7 +9947,7 @@ keyboard.inline_keyboard = {
 {text = 'اغلاق اللعبة', callback_data="/clos"},
 },
 }
-local msg_id = msg.id_/2097152/0.5
+local Msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
